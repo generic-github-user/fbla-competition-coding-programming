8369a05eab052f2daa6829ea185094a6a68f8786
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-07 17:30:03 -0500

    add comments

--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,10 @@
+// Register new account on button press
 $('button#register').click(function() {
+      // Collect account information
       var email = $('input#email').val();
       var password = $('input#password').val();
+      // Create account
       firebase.auth().createUserWithEmailAndPassword(email, password);
+      // Redirect to home back
       window.location.replace("./index.html");
 });
