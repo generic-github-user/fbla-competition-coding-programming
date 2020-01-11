9d0cdb0f87cc433249c0ec51e0d030da064166d4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-11 12:32:34 -0500

    cleanup and comments

--- src/js/write_student_info.js
+++ src/js/write_student_info.js
@@ -2,29 +2,18 @@
 // function write_student_info(selector, message, write_method, method_name) {
 function write_student_info(selector, message, doc_id) {
       // Store student info in variables
+      // Name
       var name = $(selector + ' input#student-name').val();
+      // Student number
       var number = $(selector + ' input#student-number').val();
       var grade = $(selector + ' input#student-grade').val();
       var hours = $(selector + ' input#student-total_hours').val();
 
       // Upload to database using specified write method (i.e. firebase.firestore().collection("students").add for adding a new student)
-      // console.log(write_method[method_name])
-      // if (write_method = 1) {
-      //       write_method =firebase.firestore().collection("students")
-      // }
-
       // Error is thrown if method name (.add) is included in write method
       // i.e. we cannot do firebase.firestore().collection("students").add(data)
-      // write_method[method_name]({
 
-      console.log({
-            name: name,
-            number: number,
-            grade: grade,
-            total_hours: hours,
-            created: new Date().getTime(),
-            created_by: firebase.auth().currentUser.uid
-      })
+      // Set data at ID doc_id - create document if it does not exist
       firebase.firestore().collection("students").doc(doc_id).set({
                   name: name,
                   number: number,
@@ -37,6 +26,7 @@ function write_student_info(selector, message, doc_id) {
                   // Log successful operation to console
                   console.log("Document written with ID: ", doc_id);
 
+                  // Display notification with link to view student
                   var notification = document.querySelector('.mdl-js-snackbar');
                   var snackbar_data = {
                         message: message + name + ' [' + grade + ']',
@@ -44,6 +34,7 @@ function write_student_info(selector, message, doc_id) {
                               view_student(doc_id);
                         },
                         actionText: 'View',
+                        // Timeout before hiding notification
                         timeout: 5000
                   };
                   // Display snackbar notification
