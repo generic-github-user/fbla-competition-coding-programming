60131e6597c2b146cf1aa389a992cdc770c04613
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-10 19:19:30 -0500

    comments

--- src/js/load_students.js
+++ src/js/load_students.js
@@ -1,6 +1,11 @@
+// Load list of students into dashboard
+
 db.collection('students')
+      // Sort by name
       .orderBy('name')
+      // Limit to 10 students
       .limit(10)
+      // Get values
       .get()
       .then(function(querySnapshot) {
             console.log('Retrieved student information');
@@ -9,7 +14,9 @@ db.collection('students')
                   console.log(doc.id, " => ", doc.data());
                   data = doc.data();
 
+                  // Load data as row in table
                   student_row = $('<tr id="' + doc.id + '"><td class="mdl-data-table__cell--non-numeric">' + data.name + '</td><td>' + data.number + '</td><td>' + data.grade + '</td><td>' + data.total_hours + '</td></tr>');
+                  // Bind event listener for click to display student info dialog box
                   student_row.click(() => {
                         view_student(doc.id);
                   });
@@ -17,6 +24,7 @@ db.collection('students')
                   $('#student-list').append(student_row);
             });
       })
+      // Catch errors
       .catch(function(error) {
             console.log("Error getting documents: ", error);
       });
