0bfd065ff901d6464255ecc5b3335eb644973f86
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-11 15:13:15 -0500

    add fuzzy search and results updating logic

--- src/js/help_search.js
+++ src/js/help_search.js
@@ -7,6 +7,8 @@ $('#help-button').click(function() {
 });
 help_dialog.querySelector('.close').addEventListener('click', function() {
       help_dialog.close();
+});
+
 var search_options = {
       shouldSort: true,
       threshold: 0.6,
@@ -19,4 +21,17 @@ var search_options = {
             'link'
       ]
 };
+
+var fuse = new Fuse(help_articles, search_options);
+
+$('#help-search-input').on('input', function() {
+      var search_string = $('#help-search-input').val();
+      var results = fuse.search(search_string);
+      $('#help-search-results').empty();
+      console.log(search_string)
+
+      for (var i = 0; i < results.length; i++) {
+            var url = './docs/' + results[i].link + '.html';
+            $('#help-search-results').append($('<li class="mdl-list__item mdl-list__item--three-line"><span class="mdl-list__item-primary-content"><i class="material-icons mdl-list__item-avatar">person</i><span><a href="' + url + '">' + results[i].title + '</span><span class="mdl-list__item-text-body">test</span></li>'))
+      }
 });
