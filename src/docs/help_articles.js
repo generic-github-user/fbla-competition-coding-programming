cf58c555c1c5d5b0926628d8b0e9c93d728eaa69
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-11 15:47:00 -0500

    more help article data

--- src/docs/help_articles.js
+++ src/docs/help_articles.js
@@ -1,4 +1,17 @@
 help_articles = [{
       'title': 'How to add new students',
-      'link': 'adding_students'
+      'link': 'adding_students',
+      'keywords': 'add create new students'
+}, {
+      'title': 'How to add service hours',
+      'link': 'adding_hours',
+      'keywords': 'add service hours create new'
+}, {
+      'title': 'How to remove students',
+      'link': 'removing_students',
+      'keywords': 'remove delete clear students'
+}, {
+      'title': 'How to edit student information',
+      'link': 'editing_students',
+      'keywords': 'edit modify students change alter'
 }];
