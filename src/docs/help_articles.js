0681c0193cd55934e8015710d64c6c814b3fc7f4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-01-12 15:59:01 -0500

    add more help article pointers

--- src/docs/help_articles.js
+++ src/docs/help_articles.js
@@ -11,6 +11,22 @@ help_articles = [{
       'link': 'removing_students',
       'keywords': 'remove delete clear students'
 }, {
+      'title': 'How to log in',
+      'link': 'logging_in',
+      'keywords': 'login log in sign in authenticate'
+}, {
+      'title': 'How to log out',
+      'link': 'logging_out',
+      'keywords': 'log out sign out leave'
+}, {
+      'title': 'How to search for students',
+      'link': 'finding_students',
+      'keywords': 'search students find look locate'
+}, {
+      'title': 'How to create an account',
+      'link': 'registering',
+      'keywords': 'register create account make new signup join'
+}, {
       'title': 'How to edit student information',
       'link': 'editing_students',
       'keywords': 'edit modify students change alter'
